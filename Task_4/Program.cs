99fa29a26ac24af6fc150876265e4b40aaa521a4
AVTahion <avkontakt40@gmail.com>
2019-01-29 22:44:19 +0300

    Задания 4 и 5

---
+++ Task_4/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*  4) Реализовать метод проверки логина и пароля. На вход метода подается логин и пароль. На выходе истина,
+       если прошел авторизацию, и ложь, если не прошел (Логин: root, Password: GeekBrains). Используя метод проверки
+       логина и пароля, написать программу: пользователь вводит логин и пароль, программа пропускает его дальше или не пропускает.
+       С помощью цикла do while ограничить ввод пароля тремя попытками.
+    
+    Кушмилов Александр.
+*/
+
+namespace Task_4
+{
+    class Program
+    {
+        /// <summary>
+        /// Метод проверяет входящие логин и пароль на соответствие заданным в методе логину и паролю.
+        /// </summary>
+        /// <param name="login"></param>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        static bool LoginPassCheck(string login, string password)
+        {
+            string refLogin = "root";
+            string refPass = "GeekBrains";
+            return (login == refLogin && password == refPass) ? true : false;
+        }
+
+        static void Main(string[] args)
+        {
+            bool passed = false;
+            byte i = 0;
+            do
+            {
+                Console.WriteLine("Введите логин");
+                string login = Console.ReadLine();
+                Console.WriteLine("Введите пароль");
+                string password = Console.ReadLine();
+                passed = LoginPassCheck(login, password);
+                Console.WriteLine(passed);
+                i++;
+            }
+            while (i < 3 && !passed);
+            if (!passed)
+            {
+                Console.WriteLine("Аутентификация не пройдена!");
+            }
+            else
+            {
+                Console.WriteLine("Аутентификация пройдена.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Task_5/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*  5)  а - Написать программу, которая запрашивает массу и рост человека, вычисляет его индекс массы и сообщает, нужно ли человеку похудеть, набрать вес или всё в норме.
+        б - *Рассчитать, на сколько кг похудеть или сколько кг набрать для нормализации веса.
+    
+    Александр Кушмилов
+*/
+
+namespace Task_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите Ваш Рост:");
+            string userHeight = Console.ReadLine();
+            Console.WriteLine("Введите Ваш Вес:");
+            string userWeight = Console.ReadLine();
+            double iMT = IMT(userHeight, userWeight);
+            Console.WriteLine($"Ваш индекс массы тела (ИМТ): {iMT}");
+            // норма ИМТ 18,5 — 24,99
+            double x;
+            if (iMT > 24.99)
+            {
+                Console.WriteLine("Вам нужно похудеть");
+                x = Convert.ToInt16(userWeight) - 24.99 * Math.Pow(Convert.ToDouble(userHeight) / 100, 2);
+                Console.WriteLine($"Вам нужно сбросить {x} кг.");
+            }
+            else if (iMT < 18.5)
+            {
+                Console.WriteLine("Вам нужно набрать вес");
+                x = 18.5 * Math.Pow(Convert.ToDouble(userHeight) / 100, 2) - Convert.ToInt16(userWeight);
+                Console.WriteLine($"Вам нужно набрать {x} кг.");
+            }
+            else
+            {
+                Console.WriteLine("Ваш вес в норме");
+            }
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Метод расчитывает индекс массы тела.
+        /// </summary>
+        /// <param name="userHeight"></param>
+        /// <param name="userWeight"></param>
+        /// <returns></returns>
+        private static double IMT(string userHeight, string userWeight)
+        {
+            return Convert.ToDouble(userWeight) / Math.Pow(Convert.ToDouble(userHeight) / 100, 2);
+        }
+    }
+}
